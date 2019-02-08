aa489b53535523716fecd49d2f801fe53276a7c0
RAJEEV SHEKHAR <rajeev.shekhar@capgemini.com>
2019-02-08 16:51:31 +0100

    Update index.js

--- index.js
+++ index.js
@@ -38,10 +38,12 @@ app.listen(app.get('port'), function() {
 })
 
 app.post('/webhook/', function (req, res) {
-    let messaging_events = req.body.entry[0].messaging
+    let messaging_events = req.body.entry[0].messaging;
+    console.log('messaging_events '+messaging_events);
     for (let i = 0; i < messaging_events.length; i++) {
-      let event = req.body.entry[0].messaging[i]
-      let sender = event.sender.id
+      let event = req.body.entry[0].messaging[i];
+      let sender = event.sender.id;
+      console.log('event  '+event);  
       if (event.message && event.message.text) {
         let text = event.message.text
         if (text === 'Generic') {
