afe3dc76db217a8c61fa1810c6e31bef7b58b272
RAJEEV SHEKHAR <rajeev.shekhar@capgemini.com>
2019-02-09 23:42:39 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,9 +2,50 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var request = require('request');
-//var nforceapp = require('nforce');
-var sfdc = require('./salesforce'),
-        
+var nforceapp = require('nforce');
+//var sfdc = require('./salesforce'),
+
+SF_CLIENT_ID = process.env.SF_CLIENT_ID,
+SF_CLIENT_SECRET = process.env.SF_CLIENT_SECRET,
+SF_USER_NAME = process.env.SF_USER_NAME,
+SF_PASSWORD = process.env.SF_PASSWORD;
+
+let org = nforce.createConnection({
+    clientId: SF_CLIENT_ID,
+    clientSecret: SF_CLIENT_SECRET,
+    redirectUri: 'http://localhost:3000/oauth/_callback',
+    mode: 'single',
+    autoRefresh: true
+});
+let login = () => {
+    org.authenticate({username: SF_USER_NAME, password: SF_PASSWORD}, err => {
+        if (err) {
+            console.error("Authentication error");
+            console.error(err);
+        } else {
+            console.log("Authentication successful");
+        }
+    });
+};
+let createCase = () => {
+    return new Promise((resolve, reject) => {
+        let c = nforce.createSObject('Case');
+        c.set('subject','Case from facebook bot');
+        c.set('description', 'Description from facebook bot');
+        c.set('origin', 'Web');
+        c.set('status', 'New');
+        org.insert({sobject: c}, err => {
+            if (err) {
+                console.error(err);
+                reject("An error occurred while creating a case");
+            } else {
+                resolve(c);
+            }
+        });
+    });
+
+};
+
 app.use(bodyParser.json());
 
 app.set('port', (process.env.PORT || 5000));
