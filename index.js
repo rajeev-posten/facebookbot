71af9de281923087dc0a457413ca8f4c8920cdb3
RAJEEV SHEKHAR <rajeev.shekhar@capgemini.com>
2019-02-09 14:18:19 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var request = require('request');
-var sfdc = require('./modules/salesforce'),
+//var sfdc = require('./modules/salesforce'),
         
 app.use(bodyParser.json());
 
@@ -39,7 +39,7 @@ app.post('/webhook/', function (req, res) {
                 text = "Yes I can. What can I do for you?";
             } else if(text === "Just create a dummy Case for me") {
                 text = "Okay.. creating";
-                sfdc.createCase("from fb bot","from fb bot","from fb bot");
+                //sfdc.createCase("from fb bot","from fb bot","from fb bot");
                 text = "Done. Please check";    
             }     
             sendTextMessage(sender, text);
