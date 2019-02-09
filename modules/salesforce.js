318e5e44cb2c88d4cbeebf10ad980b78dbb1a9ca
RAJEEV SHEKHAR <rajeev.shekhar@capgemini.com>
2019-02-09 14:25:42 +0100

    Update salesforce.js

--- modules/salesforce.js
+++ modules/salesforce.js
@@ -26,110 +26,14 @@ let login = () => {
     });
 };
 
-let findProperties = (params) => {
-    let where = "";
-    if (params) {
-        let parts = [];
-        if (params.id) parts.push(`id='${params.id}'`);
-        if (params.city) parts.push(`city__c='${params.city}'`);
-        if (params.bedrooms) parts.push(`beds__c=${params.bedrooms}`);
-        if (params.priceMin) parts.push(`price__c>=${params.priceMin}`);
-        if (params.priceMax) parts.push(`price__c<=${params.priceMax}`);
-        if (parts.length>0) {
-            where = "WHERE " + parts.join(' AND ');
-        }
-    }
-    return new Promise((resolve, reject) => {
-        let q = `SELECT id,
-                    title__c,
-                    address__c,
-                    city__c,
-                    state__c,
-                    price__c,
-                    beds__c,
-                    baths__c,
-                    picture__c
-                FROM property__c
-                ${where}
-                LIMIT 5`;
-        org.query({query: q}, (err, resp) => {
-            if (err) {
-                reject("An error as occurred");
-            } else {
-                resolve(resp.records);
-            }
-        });
-    });
-
-};
-
-let findPropertiesByCategory = (category) => {
-    return new Promise((resolve, reject) => {
-        let q = `SELECT id,
-                    title__c,
-                    address__c,
-                    city__c,
-                    state__c,
-                    price__c,
-                    beds__c,
-                    baths__c,
-                    picture__c
-                FROM property__c
-                WHERE tags__c LIKE '%${category}%'
-                LIMIT 5`;
-        console.log(q);
-        org.query({query: q}, (err, resp) => {
-            if (err) {
-                console.error(err);
-                reject("An error as occurred");
-            } else {
-                resolve(resp.records);
-            }
-        });
-    });
-
-};
-
-let findPriceChanges = () => {
-    return new Promise((resolve, reject) => {
-        let q = `SELECT
-                    OldValue,
-                    NewValue,
-                    CreatedDate,
-                    Field,
-                    Parent.Id,
-                    Parent.title__c,
-                    Parent.address__c,
-                    Parent.city__c,
-                    Parent.state__c,
-                    Parent.price__c,
-                    Parent.beds__c,
-                    Parent.baths__c,
-                    Parent.picture__c
-                FROM property__history
-                WHERE field = 'Price__c'
-                ORDER BY CreatedDate DESC
-                LIMIT 3`;
-        org.query({query: q}, (err, resp) => {
-            if (err) {
-                reject("An error as occurred");
-            } else {
-                resolve(resp.records);
-            }
-        });
-    });
-};
-
-
-let createCase = (propertyId, customerName, customerId) => {
+let createCase = () => {
 
     return new Promise((resolve, reject) => {
         let c = nforce.createSObject('Case');
-        c.set('subject', `Contact ${customerName} (Facebook Customer)`);
-        c.set('description', "Facebook id: " + customerId);
-        c.set('origin', 'Facebook Bot');
+        c.set('subject','Case from facebook bot');
+        c.set('description', 'Description from facebook bot');
+        c.set('origin', 'Web');
         c.set('status', 'New');
-        //c.set('Property__c', propertyId);
 
         org.insert({sobject: c}, err => {
             if (err) {
@@ -146,7 +50,4 @@ let createCase = (propertyId, customerName, customerId) => {
 login();
 
 exports.org = org;
-exports.findProperties = findProperties;
-exports.findPropertiesByCategory = findPropertiesByCategory;
-exports.findPriceChanges = findPriceChanges;
 exports.createCase = createCase;
